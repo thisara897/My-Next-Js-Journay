cd70260a4b442b5d4093ca346f3881d203e200e6
thisara897 <warshangunasinghe345@gmail.com>
2026-08-20 09:35:56 +0530

    First Commit

---
+++ prisma/migrations/20260820035102_first_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("email")
+);
---
+++ prisma/migrations/20260820035447_second_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
