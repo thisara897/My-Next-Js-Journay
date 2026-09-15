5afbae586872b58eb42fa303860b5b1333e0be78
thisara897 <warshangunasinghe345@gmail.com>
2026-09-15 07:25:33 +0530

    auth add new line

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -5,6 +5,12 @@ export async function POST(request : NextRequest){
     const body = await request.json();
     console.log(body);
 
-    prisma
+    const user = await prisma.user.findFirst(
+        {
+            where : {
+                email : body.email
+            }
+        }
+)
 
 }
